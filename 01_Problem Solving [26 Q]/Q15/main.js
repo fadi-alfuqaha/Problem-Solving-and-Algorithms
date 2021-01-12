59efe3b9fe9dd8bc72ba90c89c90a7a05f2e90c6
fadi <f.alfuqahaaa@gmail.com>
2021-01-12 13:58:51 +0000

    solve question 15

--- 01_Problem Solving [26 Q]/Q15/main.js
+++ 01_Problem Solving [26 Q]/Q15/main.js
@@ -1,4 +1,4 @@
-console.log('Problem Solving Q:15 ');
+console.log("Problem Solving Q:15 ");
 
 /*  jointCharacters*/
 
@@ -8,8 +8,16 @@ Your function should return the common characters in the same order that they ap
 Do not return duplicate characters and ignore whitespace in your returned string.
 */
 
-function jointCharacters() {
-  // YOUR CODE HERE
+function jointCharacters(first_string, second_string) {
+  let first_array = first_string.split("");
+  let second_array = second_string.split("");
+  let string = "";
+  for (let i = 0; i < first_array.length; i++) {
+    if (second_array.includes(first_string[i])) {
+      string = string + first_array[i];
+    }
+  }
+  return String.prototype.concat(...new Set(string.replace(/ /g, "")));
 }
 
 /* 
