9e39e7d764a3585e6718c7c2cb4160f18bc4348a
fadi <f.alfuqahaaa@gmail.com>
2021-01-12 05:13:17 +0000

    solve question nine

--- 01_Problem Solving [26 Q]/Q09/main.js
+++ 01_Problem Solving [26 Q]/Q09/main.js
@@ -1,4 +1,4 @@
-console.log('Problem Solving Q: 9');
+console.log("Problem Solving Q: 9");
 
 /* Longest Sequence */
 
@@ -6,8 +6,22 @@ console.log('Problem Solving Q: 9');
 Write a function that returns the length of the longest sequence of 1s in an array of 1s and 0s.
 */
 
-function longestSequence() {
-  // YOUR CODE HERE
+function longestSequence(nums) {
+  let max_sum = 0;
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 0) {
+      sum = 0;
+      continue;
+    }
+    if (nums[i] == 1) {
+      sum = sum + 1;
+    }
+    if (sum > max_sum) {
+      max_sum = sum;
+    }
+  }
+  return max_sum;
 }
 
 /* 
